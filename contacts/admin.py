8df98352a4c2088d781f5947798c9c329b45b579
Sergei Gorianin <frequenteen@gmail.com>
2021-05-06 15:18:34 +0400

    v.0.4.2
    Users chat

--- contacts/admin.py
+++ contacts/admin.py
@@ -3,8 +3,106 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
 from django.utils.decorators import method_decorator
 
+from contacts.models import Outbox, Inbox
+from users_management.models import User
+
 
 csrf_protected_method = method_decorator(csrf_protect)
 
 
+@admin.register(Outbox)
+class OutboxAdmin(admin.ModelAdmin):
+    exclude = ['sender',]
+    fields = ('receivers', 'subject', 'message',)
+    list_display = ('subject', 'receivers_list', 'date',)
 
+    def get_queryset(self, request):
+        qs = Outbox.objects.filter(sender=request.user)
+        return qs
+
+
+    def receivers_list(self, obj):
+        outbox = Outbox.objects.get(id=obj.id)
+        receivers = ''
+        for receiver in outbox.receivers.all():
+            receivers += receiver.username + '; '
+        return receivers
+
+    
+    @csrf_protected_method
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    
+    @csrf_protected_method
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    
+    def save_model(self, request, obj, form, change):
+        obj.sender = request.user
+        super().save_model(request, obj, form, change)
+        outbox = Outbox.objects.get(id=obj.id)
+        inbx = Inbox(outbox=outbox)
+        inbx.save()
+
+
+@admin.register(Inbox)
+class InboxAdmin(admin.ModelAdmin):
+    fields = [
+        'sender',
+        'receivers_list',
+        'date',
+        'subject',
+        'message',
+    ]
+
+    list_display = ('subject', 'sender', 'date',)
+    
+
+    def get_queryset(self, request):
+        qs = Inbox.objects.filter(outbox__receivers__id=request.user.id)
+        return qs
+
+
+    def sender(self, obj):
+        outbox = Outbox.objects.get(id=obj.outbox.id)
+        return outbox.sender.username
+
+
+    def receivers_list(self, obj):
+        outbox = Outbox.objects.get(id=obj.outbox.id)
+        receivers = ''
+        for receiver in outbox.receivers.all():
+            receivers += receiver.username + '; '
+        return receivers
+
+
+    def date(self, obj):
+        outbox = Outbox.objects.get(id=obj.outbox.id)
+        return outbox.date
+
+
+    def subject(self, obj):
+        outbox = Outbox.objects.get(id=obj.outbox.id)
+        return outbox.subject.name
+
+    
+    def message(self, obj):
+        outbox = Outbox.objects.get(id=obj.outbox.id)
+        return outbox.message
+
+
+    @csrf_protected_method
+    def has_add_permission(self, request):
+        return False
+
+
+    @csrf_protected_method
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+    @csrf_protected_method
+    def has_delete_permission(self, request, obj=None):
+        return False
--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.conf import settings
+
+from users_management.models import User
+
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Email subjects'
+
+
+class Outbox(models.Model):
+    sender = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL, unique=False, related_name='sender')
+    receivers = models.ManyToManyField(User, unique=False)
+    subject = models.ForeignKey(Subject, unique=False, null=False, blank=False, on_delete=models.CASCADE)
+    message = models.TextField(null=False, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.subject.name
+
+    class Meta:
+        verbose_name_plural = 'Outbox'
+
+
+class Inbox(models.Model):
+    outbox = models.ForeignKey(Outbox, unique=False, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.outbox.subject.name
+
+    class Meta:
+        verbose_name_plural = 'Inbox'
