b20d0d519675d1fc29f576bfc0e42586ff9743fc
Sergei Gorianin <frequenteen@gmail.com>
2021-05-15 12:51:47 +0400

    upd

--- general/collections/extract_functions.py
+++ general/collections/extract_functions.py
@@ -10,16 +10,28 @@ def read_file(filename: str):
 def convert_authors(authors_list: list) -> str:
     authors = ''
     if len(authors_list) > 0 and authors_list is not None:
-        for author_data in authors_list:
+        if len(authors_list) > 1:
+            for author_data in authors_list:
+                author_string = ''
+                if 'firstName' in author_data:
+                    author_string += author_data['firstName'] + ' '
+                if 'lastName' in author_data:
+                    author_string += author_data['lastName'] + ''
+                if 'name' in author_data:
+                    author_string += author_data['name'] + ''
+                
+                authors += author_string + '; '
+        elif len(authors_list) == 1:
             author_string = ''
-            if 'firstName' in author_data:
-                author_string += author_data['firstName'] + ' '
-            if 'lastName' in author_data:
-                author_string += author_data['lastName'] + ''
-            if 'name' in author_data:
-                author_string += author_data['name'] + ''
+            if 'firstName' in authors_list[0]:
+                author_string += authors_list[0]['firstName'] + ' '
+            if 'lastName' in authors_list[0]:
+                author_string += authors_list[0]['lastName'] + ''
+            if 'name' in authors_list[0]:
+                author_string += authors_list[0]['name'] + ''
             
-            authors += author_string + '; '
+            authors = author_string
+        
     return authors
 
 
